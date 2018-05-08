881cb09d89840fcc4f651d58d5c1de7e94b28551
Chris Johnston <chris@johnstoncode.com>
2018-05-08 21:39:52 +0100

    messing with docker

--- cache/c__xampp_htdocs_johnstoncode_app_views_index_index.volt.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <img src="" alt="">
-    <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-</nav>
--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp')
-    uglify = require('gulp-uglify');
-
-gulp.task('default', function() {
-
-});
