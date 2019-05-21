2034105838a06e5f7b6b7ba27f0f2c3bf015717e
Christopher <chris@johnstoncode.com>
2019-05-21 20:33:43 +0000

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ const s = serve("0.0.0.0:3000");
 
 async function main() {
   for await (const req of s) {
-    req.respond({ body: new TextEncoder().encode("Johnstoncode.com\n") });
+    req.respond({ body: new TextEncoder().encode("Johnstoncode.com now with SSL\n") });
   }
 }
 
