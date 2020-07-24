fb2b7ff8b487a69277026fe470a24893e59c300f
Chris Johnston <chris@johnstoncode.com>
2020-07-24 09:44:07 +0100

    fixed home controller typing

--- controllers/homeController.ts
+++ controllers/homeController.ts
@@ -1,6 +1,6 @@
 import { handle } from '../utils/handlebars.ts';
 import { Context } from 'https://deno.land/x/oak/mod.ts';
 
-export const home = async (context: Context) => {
-    context.response.body = await handle.renderView('home');
+export const home = async ({ response } : Context) => {
+    response.body = await handle.renderView('home');
 };
